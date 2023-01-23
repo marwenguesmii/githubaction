ea305f15337c416c30b338a2222ad54a40340cc6
Marwen <marwen.guesmi@insat.u-carthage.tn>
2023-01-23 09:15:11 +0100

    fds

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-IFS=', ' read -r -a attribut_names <<< "version, releaseDate"
-IFS=', ' read -r -a attribut_values <<< "§§è, fds"
-for i in "${!attribut_names[@]}"; do
-echo "___"
-echo "${attribut_names[i]}: ${attribut_values[i]}" >> testac.yaml
+one_file_attr_names=(url# type# size# sha512, a)
 
+echo "$one_file_attr_names"
+one_file_attr_values=(ba mar barcha)
+for j in "${!one_file_attr_names[@]}"; do
+if [ "$j" -eq "0" ]; then
+cat <<EOF >>test.yaml
+  - ${one_file_attr_names[j]}: ${one_file_attr_values[j]}
+EOF
+else
+echo "else"
+cat <<EOF >>test.yaml
+    ${one_file_attr_names[j]}: ${one_file_attr_values[j]}
+EOF
+fi
 done
-echo "*****"
-cat testac.yaml
